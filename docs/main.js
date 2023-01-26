5dcaec036f5a918e666a0b21a9e65d0e05b58c63
gKirii <kiriigaku1208@gmail.com>
2023-01-26 19:35:36 +0900

    Add reflect function

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,5 @@
 import * as THREE from "three";
 import {GUI} from "three/examples/jsm/libs/dat.gui.module";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import testVert from "./shaders/test.vert";
-import testFrag from "./shaders/test.frag";
-
-import {hoge} from "./test.jsx";
 
 let canvas, renderer, scene, camera, geometry, gui, controls;
 let target, ground;
@@ -13,6 +8,7 @@ let onPress = false;
 let nextPos;
 let vPhi, vTheta, vAlt;
 let pos;
+let cameraVelocity;
 
 const param = {
   value01: 1.0,
@@ -30,6 +26,7 @@ function init() {
   scene = new THREE.Scene();
 
   raycaster = new THREE.Raycaster();
+  cameraVelocity = new THREE.Vector3(0, 0, 0);
 }
 
 function addCamera() {
@@ -52,21 +49,8 @@ function addCamera() {
   scene.add(dLight);
   scene.add(aLight);
 
-  // const controls = new OrbitControls(camera, canvas);
-  // controls.target.set(0, 0, 0);
-  // controls.update();
-  phi = 1.0;
-  theta = 1.57;
-  alt = 10.0;
-  vPhi = 0;
-  vTheta = 0;
-  vAlt = 0;
-  pos = calcLonLatToXYZ(phi, theta, alt);
-  // nextPos = p;
-  // camera.position.set(nextPos.x, nextPos.y, nextPos.z);
-
-  target = new THREE.Object3D();
-  target.add(camera);
+  camera.position.set(0, 0.5, 5);
+  scene.add(camera);
 
   window.addEventListener("mousedown", () => {
     onPress = true;
@@ -78,65 +62,68 @@ function addCamera() {
 
   window.addEventListener("mousemove", (e) => {
     if (onPress) {
-      vPhi = vPhi + e.movementX * 0.06 * -0.03;
-      vTheta = vTheta + e.movementY * 0.06 * -0.03;
+      // vPhi = vPhi + e.movementX * 0.06 * -0.03;
+      // vTheta = vTheta + e.movementY * 0.06 * -0.03;
+      const rotateYYalue = e.movementX * 0.001;
+      const up = new THREE.Vector4(0, 1, 0, 0);
+      up.applyMatrix4(camera.matrix).normalize();
+      const axis = new THREE.Vector3(up.x, up.y, up.z);
+      camera.rotateOnAxis(axis, rotateYYalue);
     }
   });
 
-  window.addEventListener("wheel", (e) => {
-    vAlt = vAlt + e.deltaY * 0.05;
-    // alt += e.deltaY * -0.05;
-    // nextPos = calcLonLatToXYZ(phi * 2.0, theta, alt);
-    // camera.position.set(nextPos.x, nextPos.y, nextPos.z);
-  });
+  // window.addEventListener("wheel", (e) => {
+  //   vAlt = vAlt + e.deltaY * 0.05;
+  //   // alt += e.deltaY * -0.05;
+  //   // nextPos = calcLonLatToXYZ(phi * 2.0, theta, alt);
+  //   // camera.position.set(nextPos.x, nextPos.y, nextPos.z);
+  // });
 
+  const acc = 10.0;
   window.addEventListener("keydown", (e) => {
     if (e.key === "w") {
-      const p = target.position.clone();
-      target.position.set(p.x, p.y, p.z + 0.1);
-      target.updateMatrixWorld();
+      const forward = new THREE.Vector4(0, 0, 1, 0);
+      forward.applyMatrix4(camera.matrix).normalize();
+      const fv = new THREE.Vector3(forward.x, forward.y, forward.z);
+
+      const v = cameraVelocity.clone();
+      cameraVelocity = v.add(fv.multiplyScalar(-0.03 * acc));
     }
-    if (e.key === "a") {
-      const p = target.position.clone();
-      target.position.set(p.x + 0.1, p.y, p.z);
-      target.updateMatrixWorld();
-    }
+
     if (e.key === "s") {
-      const p = target.position.clone();
-      target.position.set(p.x, p.y, p.z - 0.1);
-      target.updateMatrixWorld();
-    }
-    if (e.key === "d") {
-      const p = target.position.clone();
-      target.position.set(p.x - 0.1, p.y, p.z);
-      target.updateMatrixWorld();
+      const forward = new THREE.Vector4(0, 0, 1, 0);
+      forward.applyMatrix4(camera.matrix).normalize();
+      const fv = new THREE.Vector3(forward.x, forward.y, forward.z);
+
+      const v = cameraVelocity.clone();
+      cameraVelocity = v.add(fv.multiplyScalar(0.03 * acc));
     }
   });
 }
 
 function addObject() {
-  geometry = new THREE.BoxGeometry(0.5, 1, 0.5);
+  geometry = new THREE.BoxGeometry(2, 1.5, 0.25);
   const groundGeo = new THREE.PlaneGeometry(50, 50, 10, 10);
 
-  // const mat = new THREE.ShaderMaterial({
-  //   vertexShader: testVert,
-  //   fragmentShader: testFrag,
-  // });
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load("./testTex.png");
+
   const mat = new THREE.MeshPhongMaterial();
-  const mat1 = new THREE.MeshPhongMaterial({color: 0xff0000});
+  const mat1 = new THREE.MeshPhongMaterial({map: texture});
 
   box = new THREE.Mesh(geometry, mat1);
-  box.position.set(0, -0.5, 0);
+  box.position.set(0, 0, 0);
+  box.rotateY(0.785);
   box.castShadow = true;
   box.receiveShadow = true;
-  target.add(box);
+  scene.add(box);
 
   ground = new THREE.Mesh(groundGeo, mat);
   ground.receiveShadow = true;
 
   ground.rotateX(-1.57);
-  ground.position.set(0, -1, 0);
-  scene.add(target);
+  ground.position.set(0, -0.5, 0);
+  scene.add(box);
   scene.add(ground);
 }
 
@@ -175,43 +162,95 @@ function update() {
     camera.updateProjectionMatrix();
   }
 
+  let cameraPosition = camera.position.clone();
+  cameraVelocity.multiplyScalar(0.97);
+
+  const cv = cameraVelocity.clone();
+  cameraPosition.add(cv.multiplyScalar(0.03));
+  camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+
   renderer.render(scene, camera);
-  // controls.update(0.03);
+  // controls.update();
 
-  // const cameraCurretnPos = camera.getWorldPosition();
-  // const v3 = new THREE.Vector3(0, 0, 0);
-  // box.getWorldPosition(v3);
-  // camera.position.set(nextPos.x, nextPos.y, nextPos.z);
-
-  vPhi = vPhi * 0.97;
-  vTheta = vTheta * 0.97;
-  vAlt = vAlt * 0.97;
-  phi = phi + vPhi * 0.03;
-  theta = theta + vTheta * 0.03;
-  alt = alt + vAlt * 0.03;
-
-  // console.log(theta);
+  // vPhi = vPhi * 0.97;
+  // vTheta = vTheta * 0.97;
+  // vAlt = vAlt * 0.97;
+  // phi = phi + vPhi * 0.03;
+  // theta = theta + vTheta * 0.03;
+  // alt = alt + vAlt * 0.03;
 
   //rayの設定
-  // raycaster.set(camera.getWorldPosition(), new THREE.Vector3(0, -1, 0));
-  // const intersects = raycaster.intersectObject(ground);
-  // if (intersects.length > 0) {
-  // const wCamPos = camera.getWorldPosition();
-  // const dist = wCamPos.distanceTo(intersects[0].point);
-  // pos = calcLonLatToXYZ(phi, theta, alt);
-  // console.log(`${theta}  ${dist} ${pos.y}`);
+  const v3 = new THREE.Vector3(0, 0, 0);
+  camera.getWorldPosition(v3);
+
+  const forward = new THREE.Vector4(0, 0, -1, 0);
+  forward.applyMatrix4(camera.matrix).normalize();
+  const fv = new THREE.Vector3(forward.x, forward.y, forward.z);
+  // console.log(camera.position);
+
+  raycaster.set(camera.position, fv);
+  const intersects = raycaster.intersectObject(box);
+  if (intersects.length > 0) {
+    var selectedObject = scene.getObjectByName("refLine");
+    scene.remove(selectedObject);
+    selectedObject = scene.getObjectByName("dirLine");
+    scene.remove(selectedObject);
+
+    const wn = box.localToWorld(intersects[0].face.normal.clone());
+    // console.log(wn);
+    const dir = fv.clone().normalize();
+    dir.reflect(wn.clone());
+
+    //
+    const normPoints = [];
+    normPoints.push(intersects[0].point);
+    const p1 = wn.clone().add(intersects[0].point);
+    normPoints.push(p1.add(new THREE.Vector3(0, 0.05, 0)));
+    const normMat = new THREE.LineBasicMaterial({
+      color: 0x0000ff,
+    });
+    const normGeometry = new THREE.BufferGeometry().setFromPoints(normPoints);
+    const normLine = new THREE.Line(normGeometry, normMat);
+    normLine.name = "dirLine";
+    scene.add(normLine);
+    //
+
+    //
+    const refPoints = [];
+    refPoints.push(intersects[0].point);
+    refPoints.push(
+      dir
+        .clone()
+        .add(intersects[0].point.clone())
+        .add(new THREE.Vector3(0, 0.05, 0))
+    );
+    const refMat = new THREE.LineBasicMaterial({
+      color: 0xffff00,
+    });
+    const refGeometry = new THREE.BufferGeometry().setFromPoints(refPoints);
+    const refLine = new THREE.Line(refGeometry, refMat);
+    refLine.name = "refLine";
+    scene.add(refLine);
+    //
+
+    const dist = intersects[0].point.distanceTo(camera.position);
+    if (dist < 0.2) {
+      cameraVelocity = dir.multiplyScalar(0.1);
+    }
+  }
 
   // if (dist < 0.5) {
   // const _theta = Math.acos(0.5 / alt);
   // theta = _theta * -1;
   // pos.y = -0.49;
   // vTheta = 0;
-  pos = calcLonLatToXYZ(phi, theta, alt);
+
+  // pos = calcLonLatToXYZ(phi, theta, alt);
   // }
   // }
 
-  camera.position.set(pos.x, pos.y, pos.z);
-  camera.lookAt(box.getWorldPosition());
+  // camera.position.set(pos.x, pos.y, pos.z);
+  // camera.lookAt(box.getWorldPosition());
 }
 
 function resizeRendererToDisplaySize(renderer) {
